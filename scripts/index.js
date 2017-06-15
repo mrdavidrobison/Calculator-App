fb0fbc32c8e06d2fb0bae48652dd9d2664893c1d
david robison <mrdavidrobison@gmail.com>
2017-06-14 17:33:31 -0700

    math functionality is messed up

--- scripts/index.js
+++ scripts/index.js
@@ -12,6 +12,7 @@ $(document).ready(function () {
   var nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   function getValue(input) {
+
     if (operators2.includes(inputs[inputs.length - 1] === true && input === ".")){
       console.log("Duplicate '.' ");
     }
@@ -39,13 +40,16 @@ $(document).ready(function () {
 
   $("a").on("click", function () {
     if (this.id === "deleteAll") {
-      inputs = [""];
+      inputs = ["0"];
       update();
     } else if (this.id === "backOne") {
       inputs.pop();
       update();
     } else if (this.id === "total") {
       getTotal();
+    } else if (this.id === "0" && inputs[0] === "0") {
+      inputs.shift("0"); 
+      update();
     } else {
       if (inputs[inputs.length - 1].indexOf("+", "-", "*", "/", ".") === -1) {
         getValue(this.id);
